c56dac9811b87338ca2e32070a9f58eb86d40630
Andreas Filipiuk <andreas-filipiuk@web.de>
2018-12-16 19:15:17 +0100

    Added Second Activity

--- app/src/main/java/com/example/kotlin/msgshareapp/MainActivity.kt
+++ app/src/main/java/com/example/kotlin/msgshareapp/MainActivity.kt
@@ -1,5 +1,6 @@
 package com.example.kotlin.msgshareapp
 
+import android.content.Intent
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
 import android.util.Log
@@ -16,6 +17,17 @@ class MainActivity : AppCompatActivity(){
             Log.i("MainActivity", "Button was clicked!")
 
             Toast.makeText(this, "Button was clicked!", Toast.LENGTH_SHORT).show()
+        }
+
+        btnSendInfo.setOnClickListener {
+            Log.i("MainActivity", "Second Button was clicked!")
+            val message: String = etUserMessage.text.toString()
+            Toast.makeText(this, message, Toast.LENGTH_SHORT).show()
+
+            val intent = Intent(this, SecondActivity::class.java)
+            intent.putExtra("user_input", message)
+            startActivity(intent)
+
         }
 
 
